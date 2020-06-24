a8354f3fe5c3ae276930002733afbb250c11d16f
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-06-24 20:22:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size 
-
-  end
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}
+  end.size 
 end
